c8c2d5f572f34b361cff157eeea8c7d20622fb68
Kim Jongmin <jmkim990226@gmail.com>
2025-03-08 14:41:03 +0900

    맛집 가져오기 API 추가

--- src/main/java/com/jyujyu/review/model/MenuEntity.java
+++ src/main/java/com/jyujyu/review/model/MenuEntity.java
@@ -3,11 +3,13 @@ package com.jyujyu.review.model;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.ZonedDateTime;
 
 @Builder
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "menu")
